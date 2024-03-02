1cc9cee354933a67a9bf3292ddf7602b722377df
Ilya <ilya060223@gmail.com>
2024-03-02 20:00:25 +0300

    the first task of the second stage

--- main/models.py
+++ main/models.py
@@ -3,24 +3,20 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    # Дополнительные поля пользователя
 
 class Product(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     start_date = models.DateTimeField()
     cost = models.DecimalField(max_digits=8, decimal_places=2)
-    # Дополнительные поля продукта
 
 class Lesson(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     video_link = models.URLField()
-    # Дополнительные поля урока
 
 class Group(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     members = models.ManyToManyField(User)
-    # Дополнительные поля группы
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Product, Group
+from random import shuffle
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+
+
+def users_to_groups(request, product_id):
+    products_on = Product.objects.all()
+
+    if not products_on:
+        return HttpResponse('No products')
+
+
+    product = get_object_or_404(Product, id=product_id)
+    user = request.user
+
+
+    if product.start_date > timezone.now():
+        groups = Group.objects.filter(product=product)
+
+
+        if user in groups.values_list('members', flat=True):
+            return render(request, 'already_in_group.html')
+
+
+        if not groups:
+            group = Group.objects.create(product=product, name='Group 1')
+            group.members.add(user)
+        else:
+            redistribute_groups(groups, user)
+
+
+def redistribute_groups(groups, user):
+    num_groups = len(groups)
+    total_users = sum(groups.values_list('members__count', flat=True))
+
+    target_group_size = total_users // num_groups
+
+    shuffle(groups)
+
+    for group in groups:
+        current_group_size = group.members.count()
+
+        if current_group_size < target_group_size:
+            group.members.add(user)
+            break
+
+    for group in groups:
+        group.save()
